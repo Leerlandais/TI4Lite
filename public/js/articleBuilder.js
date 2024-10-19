42fc4d6a9f0edb58e20375c446c0e61f78fa4364
leerlandais <leerlandais@gmail.com>
2024-10-19 10:16:26 +0200

    guaranteed uniqueness of recoGrid and set artGrid to be latest entries to json

---
+++ public/js/articleBuilder.js
@@ -0,0 +1,7 @@
+const articleGrid = document.getElementById("article_grid");
+const recommendedGrid = document.getElementById("recommended_grid");
+
+function postArticlesByGrid(datas, whichGrid){
+    console.log(JSON.stringify(datas));
+    console.log(whichGrid);
+}
--- public/js/jsonHandler.js
+++ public/js/jsonHandler.js
@@ -13,21 +13,55 @@ function getCategoryData(){
 
 // filtrage des articles par category
 function getByCategory(cat) {
-    console.log(cat);
     fetch("js/article-datas.json")
         .then(function(response) {
             return response.json();
         })
-        .then(function(allCats) {
-            const oneCat = allCats.filter(product => product.cat === cat);
-            createViewUsingCategory(oneCat);
-        })
+
         .catch(error => console.error('Error fetching data:', error));
 
 }
 
 function createViewUsingCategory(datas) {
     datas.forEach(function(data) {
-    console.log("THIS : "+JSON.stringify(data))
+
+        // temporary - replace with article_grid once ready
+        const categoryGrid = document.getElementById("category_grid");
+            while (categoryGrid.firstChild) {
+                categoryGrid.removeChild(categoryGrid.firstChild);
+            }
     });
 }
+
+
+function getArticleDatas(whichGrid) {
+    fetch("js/article-datas.json")
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(datas) {
+            prepareArticleDatas(datas, whichGrid);
+        })
+        .catch(error => console.error('Error fetching datas:', error));
+}
+
+function prepareArticleDatas(datas, whichGrid) {
+    const artData = datas.slice((datas.length -4),datas.length);
+    const restData = datas.filter(item => !artData.includes(item));
+    const shuffledData = restData.sort((a, b) => 0.5 - Math.random());
+    const recoData = shuffledData.slice(0,4);
+    switch(whichGrid) {
+        case "articleGrid":
+            postArticlesByGrid(artData, whichGrid);
+            break;
+        case "recommendedGrid":
+            postArticlesByGrid(recoData, whichGrid);
+            break;
+        case "categorySelection":
+
+            break;
+    }
+}
+
+getArticleDatas("articleGrid");
+getArticleDatas("recommendedGrid");
