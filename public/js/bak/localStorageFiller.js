4238548fb0a29e1a235cd7e740674069a07d46da
leerlandais <leerlandais@gmail.com>
2024-10-19 18:28:46 +0200

    removed old js files

--- public/js/bak/localStorageFiller.js
+++
@@ -1,9 +0,0 @@
-
-if (!localStorage.getItem("ITEM1")) {
-
-for(let i = 1; i <= 12; i++) {
-    const item = {id: i, sold: Math.floor(Math.random() * 20) + 1};
-    localStorage.setItem('ITEM'+i, JSON.stringify(item));
-}
-}
-
